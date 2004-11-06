ba7aaaf0805ee3f1f06b0dffc967e704c392cb2a
topping <topping@51e4faab-3f0f-0410-8d7f-865566f634a9>
2004-11-06 21:58:15 +0000

    Move PersistenceManager to own project

--- services/src/test/org/dentaku/services/ServiceTestBase.java
+++ services/src/test/org/dentaku/services/ServiceTestBase.java
@@ -1,6 +1,6 @@
 /*
- * BaseServiceTest.java
- * Copyright 2002-2004 Bill2, Inc.
+ * ServiceTestBase.java
+ * Copyright 2004-2004 Bill2, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
