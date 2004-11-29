e26683b80a3b6c32cecc204c1a9146143067831c
topping <topping@51e4faab-3f0f-0410-8d7f-865566f634a9>
2004-11-29 04:39:07 +0000

    Missed some files on last checkin.

---
+++ dentaku-persistence-service/src/java/org/dentaku/services/persistence/tranql/Schema.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2004 Your Corporation. All Rights Reserved.
+ */
+package org.dentaku.services.persistence.tranql;
+
+import org.tranql.schema.*;
+import org.tranql.query.CommandFactory;
+
+import java.util.Map;
+
+public class Schema implements org.tranql.schema.Schema {
+    public String getName() {
+        return null;
+    }
+
+    public String getPhysicalName() {
+        return null;
+    }
+
+    public Entity getEntity(String name) {
+        return null;
+    }
+
+    public Map getEntities() {
+        return null;
+    }
+
+    public CommandFactory getCommandFactory() {
+        return null;
+    }
+
+    public ModelEntity getModelEntity(String name) {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+}
