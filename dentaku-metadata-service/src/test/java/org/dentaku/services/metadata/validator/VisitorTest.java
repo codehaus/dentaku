f8cbeee9833a1844c6d879469569a5c576738620
topping <topping@51e4faab-3f0f-0410-8d7f-865566f634a9>
2004-12-17 18:09:16 +0000

    cosmetic

--- dentaku-metadata-service/src/test/java/org/dentaku/services/metadata/validator/VisitorTest.java
+++ dentaku-metadata-service/src/test/java/org/dentaku/services/metadata/validator/VisitorTest.java
@@ -44,13 +44,13 @@ public class VisitorTest extends MetadataTestBase {
             int state = 1;
             public void visit(Namespace element, Object context) throws VisitorException {
                 if (state != 3) {
-                    fail("visit(UMLClass...) was not called third");
+                    fail("visit(Namespace...) was not called third");
                 }
             }
 
             public void visit(GeneralizableElement element, Object context) throws VisitorException {
                 if (state != 2) {
-                    fail("visit(UMLClass...) was not called second");
+                    fail("visit(GeneralizableElement...) was not called second");
                 }
                 state = 3;
             }
