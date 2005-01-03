449c8e83397084786725b36db1a9b62229003d79
topping <topping@51e4faab-3f0f-0410-8d7f-865566f634a9>
2005-01-03 23:01:33 +0000

    javadoc

--- dentaku-cartridge-generator/src/java/org/dentaku/gentaku/tools/cgen/visitor/PluginOutputVisitor.java
+++ dentaku-cartridge-generator/src/java/org/dentaku/gentaku/tools/cgen/visitor/PluginOutputVisitor.java
@@ -56,17 +56,34 @@ public class PluginOutputVisitor {
         this.model = model;
     }
 
+    /**
+     * Standard visitor pattern entry point.
+     * @param mappingNode Node that we are currently visiting in the mapping document
+     * @param parentOutput Output node for document we are building.  Recursive calls gradually expand this
+     * @param parent ModelElement from UML that we are currently working on
+     * @return true if we did something worth keeping
+     */
     public boolean visit(Element mappingNode, Branch parentOutput, ModelElement parent) {
         boolean result = false;
         if (mappingNode.getName().equals("element")) {
             Element mappingXSD = (Element) xsdDoc.selectSingleNode(mappingNode.attributeValue("path"));
 
+            // the location that this node is valid, {package, class, method, field...}
             String location = mappingNode.attributeValue("location");
+            // the tag prefix we are looking for in tags named "prefix.tag"
             String prefix = mappingXSD.attributeValue("name");
-            Collection c = findElementsForLocationAndPrefix(location, prefix, parent);
-            // now iterate those...
-
-            if (!c.isEmpty()) {
+                        
+            if (location.equals("root")) {
+                // root element is a special case because we always iterate it.  But is this the only occurence not caught
+                // in the following loop?  Smells fishy
+                Element newLocalNode = DocumentHelper.createElement(mappingXSD.attributeValue("name"));
+                if (iterateElements(mappingNode, newLocalNode, parent)) {
+                    parentOutput.add(newLocalNode);
+                    result = true;
+                }
+            } else {
+                // iterate candidate ModelElements that match these criteria
+                Collection c = findElementsForLocationAndPrefix(location, prefix, parent);
                 for (Iterator elementIterator = c.iterator(); elementIterator.hasNext();) {
                     ModelElementImpl element = (ModelElementImpl) elementIterator.next();
                     if (element instanceof Namespace || element instanceof Feature) {
@@ -76,37 +93,51 @@ public class PluginOutputVisitor {
                     }
 
                     Element newLocalNode = DocumentHelper.createElement(mappingXSD.attributeValue("name"));
+                    // @todo note that in older versions of this code, we used to keep elements that properly rendered, even if a node did not have
+                    // any attributes that rendered.  This was removed organically in the process of tracking down problems, but it may be that with
+                    // that problem solved that the old behavior is the correct behavior.  If it is, the special case noise when a root element is
+                    // being rendered can probably be removed as well
                     if (iterateAttributes(mappingXSD, element, newLocalNode)) {
                         iterateElements(mappingNode, newLocalNode, parent);
                         parentOutput.add(newLocalNode);
                         result = true;
                     }
                 }
-            } else if (location.equals("root")) {
-                Element newLocalNode = DocumentHelper.createElement(mappingXSD.attributeValue("name"));
-                if (iterateElements(mappingNode, newLocalNode, parent)) {
-                    parentOutput.add(newLocalNode);
-                    result = true;
-                }
             }
         }
         return result;
     }
 
+    /**
+     * Iterate all attributes for a node.  The result of this call is used to determine whether child elements are rendered
+     * as well.
+     * @param mappingXSD The Schema document node for matching the element we are looking at
+     * @param element The model element that we are examining
+     * @param newLocalNode Output node for document we are building.  Recursive calls at higher level gradually expand this
+     * @return true if we should keep this node
+     */
     private boolean iterateAttributes(Element mappingXSD, ModelElementImpl element, Element newLocalNode) {
         boolean result = false;
         for (Iterator it = mappingXSD.selectNodes("*/xs:attribute").iterator(); it.hasNext();) {
-            Element node1 = (Element) it.next();
-            String attrKey = mappingXSD.attributeValue("name") + "." + node1.attributeValue("name");
-            TaggedValueImpl taggedValue = (TaggedValueImpl) element.getTaggedValue(attrKey);
+            Element node = (Element) it.next();
+            String tagName = node.attributeValue("name");
+            String tagClass = mappingXSD.attributeValue("name");
+            TaggedValueImpl taggedValue = (TaggedValueImpl) element.getTaggedValue(tagClass + "." + tagName);
             if (taggedValue != null) {
-                ((Element) newLocalNode).addAttribute(node1.attributeValue("name"), taggedValue.getValue());
+                newLocalNode.addAttribute(tagName, taggedValue.getValue());
                 result = true;
             }
         }
         return result;
     }
 
+    /**
+     * Iterate all the elements of the mappingNode by visiting each one of them.
+     * @param mappingNode The node of the mapping document
+     * @param newLocalNode Output node for document we are building.  Recursive calls gradually expand this
+     * @param parent The parent element from the UML model that we are parsing in parellel
+     * @return true if we did anything of value
+     */
     private boolean iterateElements(Element mappingNode, Element newLocalNode, ModelElement parent) {
         boolean result = false;
         for (Iterator it = mappingNode.elements().iterator(); it.hasNext();) {
@@ -117,27 +148,19 @@ public class PluginOutputVisitor {
     }
 
     /**
-     * Returns all model objects that are in a specified location and containing a tag with the specified prefix
+     * Returns all model objects (read: candidates) that are in a specified location and containing a tag with the specified prefix
      *
      * @param location Specifies the model base class to search through
      * @param prefix   Node is selected if it contains a tag of that prefix
-     * @param parent
+     * @param parent ModelElement in the UML model that we are currently considering
      * @return Nodes matching criteria
      */
     private Collection findElementsForLocationAndPrefix(String location, final String prefix, final ModelElement parent) {
+        // special case if parent is not an element container, the list of candidate elements is just this element
         if (parent != null && !(parent instanceof Namespace)) {
             return Collections.singletonList(parent);
         }
-        Collection elements = Collections.EMPTY_LIST;
-        if (location.equals("package")) {
-            elements = model.getModelManagement().getUmlPackage().refAllOfType();
-        } else if (location.equals("class")) {
-            elements = model.getCore().getUmlClass().refAllOfType();
-        } else if (location.equals("method")) {
-            elements = model.getCore().getOperation().refAllOfType();
-        } else if (location.equals("field")) {
-            elements = model.getCore().getAttribute().refAllOfType();
-        }
+        Collection elements = getCandidateElementsForLocation(location);
 
         Collection result = CollectionUtils.select(elements, new Predicate() {
             public boolean evaluate(Object object) {
@@ -159,6 +182,25 @@ public class PluginOutputVisitor {
     }
 
     /**
+     * Returns crosscut of all model elements for a type.
+     * @param location The location we are wanting to look in, {package, class, method, field...}
+     * @return All nodes in the model from that location
+     */
+    private Collection getCandidateElementsForLocation(String location) {
+        Collection elements = Collections.EMPTY_LIST;
+        if (location.equals("package")) {
+            elements = model.getModelManagement().getUmlPackage().refAllOfType();
+        } else if (location.equals("class")) {
+            elements = model.getCore().getUmlClass().refAllOfType();
+        } else if (location.equals("method")) {
+            elements = model.getCore().getOperation().refAllOfType();
+        } else if (location.equals("field")) {
+            elements = model.getCore().getAttribute().refAllOfType();
+        }
+        return elements;
+    }
+
+    /**
      * DocumentFactory class for dom4j.  The base implementation of the visitor was insufficient four our needs, but not sure yet
      * what the information we needed for a custom AST.  So the best thing was to keep the AST in dom4j (giving full access to the
      * actual XML) but change the accept method through a change to the factory.
@@ -170,14 +212,22 @@ public class PluginOutputVisitor {
     }
 
     /**
-     * The class that is instantiated by the factory.  Really only does something with the accept method, adding a couple of custom
-     * state parameters we need and *not* calling all the childen of a node as the defualt impl does.
+     * The class that is instantiated by the factory.  Really only does something with the accept method.
      */
     public static class LocalDefaultElement extends DefaultElement {
         public LocalDefaultElement(QName qname) {
             super(qname);
         }
 
+        /**
+         * Basic accept method, adding a couple of state parameters we need and *not* calling all the childen of a node as the
+         * defualt impl does.  We want to be discriminate about how we do that and update state parameters for children (using the
+         * stack in the process for recursive state)
+         * @param visitor
+         * @param newParent
+         * @param parent
+         * @return
+         */
         public boolean accept(PluginOutputVisitor visitor, Branch newParent, ModelElement parent) {
             return visitor.visit(this, newParent, parent);
         }
