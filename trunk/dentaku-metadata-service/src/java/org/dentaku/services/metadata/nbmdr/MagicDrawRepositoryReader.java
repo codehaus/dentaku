b1322811766a95028034d00ea6e1df306e34c85b
stpdave <stpdave@51e4faab-3f0f-0410-8d7f-865566f634a9>
2005-01-30 17:38:48 +0000

    Unused import removed

--- trunk/dentaku-metadata-service/src/java/org/dentaku/services/metadata/nbmdr/MagicDrawRepositoryReader.java
+++ trunk/dentaku-metadata-service/src/java/org/dentaku/services/metadata/nbmdr/MagicDrawRepositoryReader.java
@@ -31,6 +31,8 @@ import javax.jmi.model.ModelPackage;
 import javax.jmi.model.MofPackage;
 import javax.jmi.reflect.RefPackage;
 import javax.jmi.xmi.MalformedXMIException;
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
