cae80f34d241abd21f977ad7de8a553d05ef6b7b
topping <topping@51e4faab-3f0f-0410-8d7f-865566f634a9>
2005-04-17 20:46:05 +0000

    Finish changes for DENTAKU-22

--- trunk/dentaku-cartridge-generator/src/test/java/org/dentaku/gentaku/tools/cgen/XMIGenTest.java
+++ trunk/dentaku-cartridge-generator/src/test/java/org/dentaku/gentaku/tools/cgen/XMIGenTest.java
@@ -18,6 +18,9 @@ package org.dentaku.gentaku.tools.cgen;
 
 import junit.framework.TestCase;
 import org.dentaku.gentaku.tools.cgen.xmi.XMIGenTask;
+import org.dentaku.gentaku.tools.cgen.xmi.Module;
+
+import java.io.File;
 
 public class XMIGenTest extends TestCase {
     public XMIGenTask gen;
@@ -25,9 +28,12 @@ public class XMIGenTest extends TestCase {
     protected void setUp() throws Exception {
         super.setUp();
         gen = new XMIGenTask();
-        gen.setMapping("dentaku-cartridge-generator/src/java/org/dentaku/gentaku/cartridge/jdo/xml/mapping.xml");
-        gen.setSchema("dentaku-cartridge-generator/src/java/org/dentaku/gentaku/cartridge/jdo/xml/jdo_2_0.xsd");
+        String root = System.getProperty("dentaku.rootdir");
+        Module m = new Module();
+        m.setMapping(new File(root, "dentaku-cartridge-generator/src/java/org/dentaku/gentaku/cartridge/jdo/xml/mapping.xml").getPath());
+        m.setSchema(new File(root, "dentaku-cartridge-generator/src/java/org/dentaku/gentaku/cartridge/jdo/xml/jdo_2_0.xsd").getPath());
         gen.setDestdir("/tmp");
+        gen.addModule(m);
     }
 
     public void testXMIGen() throws Exception {
